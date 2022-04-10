b655a4e3f42ad354f0d5fe332e76b6ae6d5db594
malikkorucu <malikkorucu@gmail.com>
2022-04-11 02:16:28 +0300

    console cleared

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -34,7 +34,6 @@ export class ProductController {
     @Post('/product')
     @UseBefore(upload.fields([{ name: 'image', maxCount: 5 }, { name: 'other_images', maxCount: 5 }]))
     public async addProduct(@Body() product: any, @Res() res: Response): Promise<Response> {
-        console.log("product",product)
         const data = await this.service.createProduct(product);
         return res.json(data);
     }
